77a40b1b60cb6ad60f76c25c3717149be578ced8
AndreyFun <kozlovandrey@ukr.net>
2023-10-09 20:47:52 +0200

    my first_lessons

---
+++ first_lessons.py
@@ -0,0 +1,20 @@
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+oper = input("Enter operator: ")
+if oper == "+":
+    c = a + b 
+elif oper == "-":
+    c = a - b     
+elif oper == "*":
+    c = a * b 
+elif oper == "/":
+    c = a / b       
+else:  
+    print("Incorrect operator")
+    exit(1) 
+print(f"result is : {c}")
+
+
+
+
+
